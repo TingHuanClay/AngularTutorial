3088eae210047354f61ddef3cab5f79b803798cf
TingHuanclay <claytinghuan@gmail.com>
2020-02-10 18:33:31 +0800

    feat: Add Dashboard and rounting funciton
    - added the Angular router to navigate among different components.
    - turned the AppComponent into a navigation shell with <a> links and a <router-outlet>.
    - configured the router in an AppRoutingModule
    - defined simple routes, a redirect route, and a parameterized route.
    - used the routerLink directive in anchor elements.
    - refactored a tightly-coupled master/detail view into a routed detail view.
    - used router link parameters to navigate to the detail view of a user-selected hero.
    - shared the HeroService among multiple components.

--- angular-tour-of-heroes/src/app/app-routing.module.ts
+++ angular-tour-of-heroes/src/app/app-routing.module.ts
@@ -1,6 +1,9 @@
-import { NgModule } from '@angular/core';
+import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HeroesComponent } from './heroes/heroes.component';
+ 
+import { DashboardComponent }   from './dashboard/dashboard.component';
+import { HeroesComponent }      from './heroes/heroes.component';
+import { HeroDetailComponent }  from './hero-detail/hero-detail.component';
 
 /*
 A typical Angular Route has following two properties:
@@ -9,10 +12,11 @@ path: a string that matches the URL in the browser address bar.
 component: the component that the router should create when navigating to this route.
 */
 const routes: Routes = [
-  {
-    path: 'heroes',
-    component: HeroesComponent
-  }
+  // redirects a URL that fully matches the empty path to the route whose path is '/dashboard'
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'detail/:id', component: HeroDetailComponent },
+  { path: 'heroes', component: HeroesComponent }
 ];
 
 @NgModule({
@@ -22,7 +26,6 @@ const routes: Routes = [
   //   CommonModule
   // ]
   imports: [ RouterModule.forRoot(routes) ],
-
   exports: [ RouterModule ]
 })
 export class AppRoutingModule { }
