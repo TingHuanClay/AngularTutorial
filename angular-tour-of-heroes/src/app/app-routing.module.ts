243e94af8f08924c6f00218a9ea194af6b28e59c
TingHuanclay <claytinghuan@gmail.com>
2020-02-10 17:31:50 +0800

    feat: Add Routing Module into the app

--- angular-tour-of-heroes/src/app/app-routing.module.ts
+++ angular-tour-of-heroes/src/app/app-routing.module.ts
@@ -1,12 +1,28 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
 
+/*
+A typical Angular Route has following two properties:
 
+path: a string that matches the URL in the browser address bar.
+component: the component that the router should create when navigating to this route.
+*/
+const routes: Routes = [
+  {
+    path: 'heroes',
+    component: HeroesComponent
+  }
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  // Generally don't declare components in a routing module
+  // declarations: [],
+  // imports: [
+  //   CommonModule
+  // ]
+  imports: [ RouterModule.forRoot(routes) ],
+
+  exports: [ RouterModule ]
 })
 export class AppRoutingModule { }
