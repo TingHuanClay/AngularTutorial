00e123a5f8a29d5277981ffa5866b0bbe548b3f0
TingHuanclay <claytinghuan@gmail.com>
2020-02-11 09:20:21 +0800

    fix: fix go back funciton doesn't work issue

--- angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
+++ angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Hero } from '../hero';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 
+import { Hero } from '../hero';
 import { HeroService }  from '../hero.service';
 
 @Component({
@@ -29,4 +29,7 @@ export class HeroDetailComponent implements OnInit {
       .subscribe(hero => this.hero = hero);
   }
 
+  goBack(): void {
+    this.location.back();
+  }
 }
